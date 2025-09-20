33039cc9991e791ba5b267f46caa52175fccb475
kohinurfatema <kohinurfatema35@gmail.com>
2025-09-20 03:56:11 -0700

    add css ,using state btn

--- 17-9-2025-M-38/countries-to-visit/src/components/Countries/Countries.jsx
+++ 17-9-2025-M-38/countries-to-visit/src/components/Countries/Countries.jsx
@@ -1,9 +1,17 @@
 
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
 import './Countries.css'
 
 const countries = ({CountriesPromise}) => {
+
+    const [visitedCountries , setVisitedCountries] = useState([]);
+
+    const updateVisitedCountries = (country) => {
+    
+        const newVisitedCountries = [...visitedCountries,country];
+        setVisitedCountries(newVisitedCountries);
+    }
 
     const countriesData = use(CountriesPromise);
     const allCountries = countriesData.countries;
@@ -11,9 +19,14 @@ const countries = ({CountriesPromise}) => {
     return (
         <div>
             <h1>In Different Countries:{allCountries.length}</h1>
+            <h3>Total Country Visited:</h3>
            <div className='countries'>
              {
-                allCountries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+                allCountries.map(country => <Country 
+                     key={country.cca3.cca3} 
+                     country={country}
+                     updateVisitedCountries={updateVisitedCountries}>
+                    </Country>)
              }
            </div>
         </div>
--- 17-9-2025-M-38/countries-to-visit/src/components/Country/Country.jsx
+++ 17-9-2025-M-38/countries-to-visit/src/components/Country/Country.jsx
@@ -1,20 +1,26 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import './country.css';
 
-const Country = ({country}) => {
+const Country = ({country,updateVisitedCountries}) => {
+
+    const [visited, setVisited] = useState(false);
+   // console.log(updateVisitedCountries);
 
     const handleVisited = () => {
-        console.log('button cliked');
+       setVisited(!visited);
+       updateVisitedCountries(country);
     }
 
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
             <p>Area: {country.area.area} {country.area.area>300000 ? 'Big Country': 'Small Country'}</p>
-            <button onClick={handleVisited}>Not Visited</button>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 };
